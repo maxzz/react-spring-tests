6e08c87a9f0bae6b006f4f4a7dcbd448d4c394bf
Max Zakharzhevskiy <maxzz@msn.com>
2021-11-21 17:26:27 -0800

    WIP. case 01. simplest move

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import React, { useState } from 'react';
 import './App.css';
+import { a, useSpring } from '@react-spring/web';
 
 function Case01() {
-    const [open, setOpen] = useState(false);
+    const [run, setRun] = useState(false);
+    const bind = useSpring({
+        x: run ? 100 : 0
+    });
     return (
         <div className="w-full h-96 grid grid-rows-[auto,minmax(0,1fr)] bg-red-400">
             <div className="flex justify-end">
-                <button className="px-4 py-2 bg-red-400 border border-red-800 rounded active:scale-[.97]" onClick={() => setOpen(!open)}>
+                <button className="px-4 py-2 bg-red-400 border border-red-800 rounded active:scale-[.97]"
+                    onClick={() => {
+                        setRun(!run);
+                    }}
+                >
                     Run
                 </button>
             </div>
-            {open &&
-                <div className="">
-                </div>
-            }
+            <div className="">
+                <a.div style={{ ...bind }} className="w-32 h-20 border rounded-md bg-purple-400/50 border-purple-800">
+                </a.div>
+            </div>
         </div>
     );
 }
