5f263b7541e4f98fddc546ea9cd5420263e8bc1c
Max Zakharzhevskiy <maxzz@msn.com>
2021-11-21 16:13:57 -0800

    Initial add tailwind support

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-        aa
-    </div>
-  )
+    return (
+        <div className="App bg-red-400">
+            aa
+        </div>
+    );
 }
 
-export default App
+export default App;
