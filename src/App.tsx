34042f09f21171be6b69f187b731977b2f8fbf57
Max Zakharzhevskiy <maxzz@msn.com>
2021-11-21 17:00:29 -0800

    WIP. test buton down state variations

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,21 @@ function Case01() {
     return (
         <div className="w-full h-96 grid grid-rows-[auto,minmax(0,1fr)] bg-red-400">
             <button className="px-4 py-2 bg-red-400 border border-red-800 rounded" onClick={() => setOpen(!open)}>
+                <div className="active:scale-[.97] active:font-bold">Run</div>
+            </button>
+            {open &&
+                <div className="">
+                </div>
+            }
+        </div>
+    );
+}
+
+function Case02() {
+    const [open, setOpen] = useState(false);
+    return (
+        <div className="w-full h-96 grid grid-rows-[auto,minmax(0,1fr)] bg-red-400">
+            <button className="px-4 py-2 bg-red-400 border border-red-800 rounded" onClick={() => setOpen(!open)}>
                 Open case 01
             </button>
             {open &&
@@ -22,9 +37,9 @@ function App() {
             <div className="h-screen grid place-items-center bg-[salmon] text-red-800">
                 <div className="mx-4 grid grid-cols-[repeat(2,minmax(400px,1fr))] gap-4">
                     <Case01 />
-                    <Case01 />
-                    <Case01 />
-                    <Case01 />
+                    <Case02 />
+                    <Case02 />
+                    <Case02 />
                 </div>
             </div>
         </React.Fragment>
