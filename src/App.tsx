24f57a472591ede41a02058da3c89a1dc624f946
Max Zakharzhevskiy <maxzz@msn.com>
2021-11-21 16:43:25 -0800

    add initial case skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-    const [count, setCount] = useState(0);
-
+function Case01() {
+    const [open, setOpen] = useState(false);
     return (
-        <div className="App bg-red-400">
-            aa
+        <div className="h-96 grid grid-rows-[auto,minmax(0,1fr)] bg-red-400">
+            <button className="px-4 py-2 text-red-800 bg-red-400 border border-red-800 rounded" onClick={() => setOpen(!open)}>
+                Open case 01
+            </button>
+            {open &&
+                <div className="">11</div>
+
+            }
         </div>
+    );
+}
+
+function App() {
+    return (
+        <React.Fragment>
+            <div className="h-screen grid place-items-center bg-[salmon]">
+                <Case01 />
+            </div>
+        </React.Fragment>
     );
 }
 
