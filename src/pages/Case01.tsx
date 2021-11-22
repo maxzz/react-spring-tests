c2dec8243007b5e57cf04cfd54fbeed55cef181d
Max Zakharzhevskiy <maxzz@msn.com>
2021-11-21 18:18:05 -0800

    WIP. case 01. add auto reset

--- src/pages/Case01.tsx
+++ src/pages/Case01.tsx
@@ -4,15 +4,24 @@ import { useMeasure } from 'react-use';
 
 export function Case01() {
     const [running, setRunning] = React.useState(false);
+    const [auto, setAuto] = React.useState(false);
     const [containerRef, { width: containerWidth }] = useMeasure<HTMLDivElement>();
     const [elementRef, { width: elementWidth }] = useMeasure<HTMLDivElement>();
     const bind = useSpring({
         x: running ? containerWidth - elementWidth - 2 : 0,
         config: { ...config.wobbly, mass: .2, clamp: true },
+        onRest: () => {
+            console.log('done');
+            auto && setRunning(!running);
+        }
     });
     return (
         <div className="w-full h-96 grid grid-rows-[auto,minmax(0,1fr)] bg-red-400">
-            <div className="flex justify-end">
+            <div className="flex justify-end space-x-4">
+                <label className="self-end text-sm flex items-center space-x-1">
+                    <input className="w-4 h-4" type="checkbox" checked={auto} onChange={(event) => setAuto(event.target.checked)} />
+                    <span className="">auto</span>
+                </label>
                 <button className="px-4 py-2 bg-red-400 border border-red-800 rounded active:scale-[.97]"
                     onClick={() => {
                         setRunning(!running);
