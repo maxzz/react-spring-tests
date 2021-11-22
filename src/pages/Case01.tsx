73b3371b80d51ef8c26534c1d1382d15d1914698
Max Zakharzhevskiy <maxzz@msn.com>
2021-11-21 17:46:46 -0800

    WIP. case 01. add movement tweaks

--- src/pages/Case01.tsx
+++ src/pages/Case01.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import { a, useSpring } from '@react-spring/web';
+import { a, config, useSpring } from '@react-spring/web';
 import { useMeasure } from 'react-use';
 
 export function Case01() {
-    const [run, setRun] = React.useState(false);
+    const [running, setRunning] = React.useState(false);
     const [containerRef, { width: containerWidth }] = useMeasure<HTMLDivElement>();
     const [elementRef, { width: elementWidth }] = useMeasure<HTMLDivElement>();
     const bind = useSpring({
-        x: run ? containerWidth - elementWidth : 0
+        x: running ? containerWidth - elementWidth - 2 : 0,
+        config: { ...config.wobbly, mass: .2, clamp: true },
     });
     return (
         <div className="w-full h-96 grid grid-rows-[auto,minmax(0,1fr)] bg-red-400">
             <div className="flex justify-end">
                 <button className="px-4 py-2 bg-red-400 border border-red-800 rounded active:scale-[.97]"
                     onClick={() => {
-                        setRun(!run);
+                        setRunning(!running);
                     }}
                 >
                     Run
                 </button>
             </div>
-            <div ref={containerRef} className="mt-4">
+            <div ref={containerRef} className="mt-4 p-1 border border-dotted">
                 <a.div ref={elementRef} style={{ ...bind }} className="w-32 h-20 border rounded-md bg-purple-400/50 border-purple-800">
                 </a.div>
             </div>
