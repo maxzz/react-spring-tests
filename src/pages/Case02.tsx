6cbd5e26aed98cdcdf98d127aa7ee0328f889b8a
Max Zakharzhevskiy <maxzz@msn.com>
2021-11-23 18:48:19 -0800

    WIP. case 02. add wobbly mass switch

--- src/pages/Case02.tsx
+++ src/pages/Case02.tsx
@@ -6,6 +6,7 @@ import { SimpleCheckbox } from './Case01';
 export function Case02() {
     const [running, setRunning] = React.useState(false);
     const [auto, setAuto] = React.useState(false);
+    const [wobbly, setWobbly] = React.useState(false);
 
     const [containerRef, { width: containerWidth }] = useMeasure<HTMLDivElement>();
     const [elementRef, { width: elementWidth }] = useMeasure<HTMLDivElement>();
@@ -14,7 +15,7 @@ export function Case02() {
         x: running ? containerWidth - elementWidth - 2 : 0,
         config: {
             ...config.wobbly,
-            mass: 2,
+            mass: wobbly ? 2 : .2, // this change will not have effect during running animation
             clamp: true,
         },
         onRest: () => {
@@ -26,8 +27,18 @@ export function Case02() {
         <div className="w-full h-96 grid grid-rows-[auto,minmax(0,1fr)] bg-red-400">
 
             {/* Controls */}
-            <div className="flex justify-end space-x-4">
-                <SimpleCheckbox label="auto reset animation" value={auto} onChange={setAuto} />
+            <div className="flex justify-between space-x-4">
+
+                <div className="ml-4 mt-2 flex flex-col">
+                    <div className="text-xs">
+                        container: {containerWidth.toFixed(0)} element: {elementWidth.toFixed(0)}
+                    </div>
+                    <div className="flex items-center space-x-4">
+                        <SimpleCheckbox label="wobbly" value={wobbly} onChange={setWobbly} />
+                        <SimpleCheckbox label="auto reset animation" value={auto} onChange={setAuto} />
+                    </div>
+                </div>
+
                 <button className="px-4 py-2 w-16 bg-red-400 border border-red-800 rounded active:scale-[.97]"
                     onClick={() => {
                         setRunning(!running);
@@ -43,7 +54,7 @@ export function Case02() {
                     ref={elementRef}
                     style={{
                         ...bind,
-                        width: bind.x.to({ range: [0, 1], output: [0.6, 2], })
+                        width: bind.x.to({ range: [0, 1], output: [0, 2], })
                     }}
                     className="w-32 h-20"
                 >
