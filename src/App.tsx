ded485af7bbd47a57957481590107e0178f6cc92
Max Zakharzhevskiy <maxzz@msn.com>
2021-11-24 23:46:40 -0800

    WIP. router. add page transitions

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ import { Case03 } from './pages/Case03';
 import { Case04 } from './pages/Case04';
 import { Case0X } from './pages/Case0X';
 
-import { BrowserRouter as Router, Switch, Route, Redirect, Link, LinkProps } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect, Link, LinkProps, useLocation } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 function PageA() {
     return (
@@ -28,7 +29,7 @@ function PageA() {
 function PageB() {
     return (
         <React.Fragment>
-            111
+            <div className="">111</div>
         </React.Fragment>
     );
 }
@@ -42,6 +43,7 @@ function NavLink(props: LinkProps & React.RefAttributes<HTMLAnchorElement>) {
 }
 
 function AppRoutes() {
+    let location = useLocation();
     return (
         <div className="h-screen flex flex-col bg-[tomato] text-red-800">
             <nav className="p-4 flex justify-end space-x-4">
@@ -49,10 +51,14 @@ function AppRoutes() {
                 <NavLink to="/springs">Springs</NavLink>
             </nav>
             <div className="flex-1 w-full h-full bg-[salmon]">
-                <Switch>
-                    <Route path="/spring" children={PageA} />
-                    <Route path="/springs" children={PageB} />
-                </Switch>
+                <TransitionGroup>
+                    <CSSTransition key={location.pathname} classNames="fade" timeout={300}>
+                        <Switch>
+                            <Route path="/spring" children={PageA} />
+                            <Route path="/springs" children={PageB} />
+                        </Switch>
+                    </CSSTransition>
+                </TransitionGroup>
             </div>
         </div>
     );
