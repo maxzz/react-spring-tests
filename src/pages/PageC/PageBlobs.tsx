56240e381afefc4b212810307adf7845d3311f5c
Max Zakharzhevskiy <maxzz@msn.com>
2021-12-16 19:13:45 -0800

    WIP. PageCBlobs. add goo toggle

--- src/pages/PageC/PageBlobs.tsx
+++ src/pages/PageC/PageBlobs.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { a, useTrail, Spring, config } from "@react-spring/web";
 import './PageBlobs.scss';
 import { classNames } from '../../utils/classnames';
+import { atom, useAtom } from 'jotai';
 
 const configFast = { tension: 1200, friction: 40 };
 const configSlow = { mass: 10, tension: 200, friction: 50 };
@@ -50,25 +51,23 @@ function Blobs() {
         config: configSlow,
     }));
     return (
-        <>
-            <div className="absolute inset-0 overflow-hidden border border-gray-900/20">
-                <div
-                    className="blobs absolute inset-0"
-                    style={{ filter: 'url(#goo-filter)' }}
-                    onMouseMove={e => api.start({ xy: [e.clientX, e.clientY] })}
-                >
-                    {trail.map((props, index) => (
-                        <a.div
-                            key={index}
-                            style={{ transform: props.xy.to(interpolate) }}
-                            className={classNames(
-                                "bg-purple-700 opacity-60"
-                            )}
-                        />
-                    ))}
-                </div>
+        <div className="absolute inset-0 overflow-hidden border border-gray-900/20">
+            <div
+                className="blobs absolute inset-0"
+                style={{ filter: 'url(#goo-filter)' }}
+                onMouseMove={e => api.start({ xy: [e.clientX, e.clientY] })}
+            >
+                {trail.map((props, index) => (
+                    <a.div
+                        key={index}
+                        style={{ transform: props.xy.to(interpolate) }}
+                        className={classNames(
+                            "bg-purple-700 opacity-60"
+                        )}
+                    />
+                ))}
             </div>
-        </>
+        </div>
     );
 }
 
@@ -83,14 +82,21 @@ function FilterGoo() {
     );
 }
 
+const useGooAtom = atom(false);
+
 function PageContent() {
+    const [useGoo, setUseGoo] = useAtom(useGooAtom);
     return (
         <Spring from={{ opacity: 0, scale: 0 }} to={{ opacity: 1, scale: 1 }} config={config.wobbly}>
             {(props) => (
                 <a.div style={props}>
-                    <div className="grid place-items-center gap-4">
+                    <div className="grid place-items-center">
                         <div className="text-7xl opacity-50">⚗</div>
-                        <div className="text-3xl opacity-50 font-black uppercase tracking-tighter">Goo Blobs</div>
+                        <div className="mt-4 text-3xl opacity-50 font-black uppercase tracking-tighter">Goo Blobs</div>
+                        <label className="flex items-center space-x-1">
+                            <input type="checkbox" className="form-checkbox select-none" />
+                            <div className="">Use filter</div>
+                        </label>
                     </div>
                 </a.div>
             )}
@@ -101,9 +107,9 @@ function PageContent() {
 export function PageCBlobs() {
     return (
         <div className="page grid place-items-center relative bg-red-700/20">
-            <PageContent />
             <FilterGoo />
             <Blobs />
+            <PageContent />
         </div>
     );
 }
