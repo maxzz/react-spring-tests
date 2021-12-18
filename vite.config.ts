df3bb2cef0b7a340c42a3844cbed940597c0418e
Max Zakharzhevskiy <maxzz@msn.com>
2021-12-18 14:19:51 -0800

    Add packages @rollup/plagin-replace, @rollup/plugin-url, and rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
+import url from '@rollup/plugin-url';
+import replace from '@rollup/plugin-replace';
 
 // https://vitejs.dev/config/
 export default defineConfig({
