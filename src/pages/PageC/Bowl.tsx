e1a0525e07fb996e2e50ec3746e4b841088ac867
Max Zakharzhevskiy <maxzz@msn.com>
2021-12-19 23:49:52 -0800

    WIP. bubbles. separated to bowl

---
+++ src/pages/PageC/Bowl.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import { rnd } from '../../utils/numbers';
+
+const bubblesAnim = keyframes`
+   0% {
+      opacity: 0;
+      background-color: teal;
+      scale: 0.1;
+   }
+   20% { //show and hint at moving
+      opacity: 0.25;
+      transform: translate(-30%, 40%);
+      scale: 0.5;
+   }
+   40% { //show and hint at moving
+      opacity: 1;
+      transform: translate(30%, -20%);
+      scale: 0.2;
+   }
+   60%{
+      background-color: #fff7;
+      transform: translate(-60%, -20%);
+      scale: 1;
+   }
+   100% {
+      opacity: 0;
+      transform: translate(0, -500%); //big bubbles move faster
+   }
+`;
+
+const BubbleChild = styled.div`
+    position: absolute;
+    //background-color: red;
+    background-color: #fff7;
+    opacity: 0;
+    border-radius: 100%;
+    animation: ${bubblesAnim} 3s ease-in infinite;
+`;
+
+export function Bubbles() {
+    const [total, setTotal] = React.useState(10);
+    React.useEffect(() => {
+        const int = setInterval(() => setTotal(rnd(total - 1, total + 1)), 5000);
+        return () => clearInterval(int);
+    }, []);
+    return (
+        <div className="absolute left-[15.5%] top-[12.5%] w-[48.5%] h-[66%] overflow-hidden rounded-full bg-blue-600/20">
+            {/* <BubbleChild $left={rnd(10, 80)} $top={rnd(60, 80)} $size={rnd(40, 80) / 10} $delay={-rnd(0, 30) / 10} key={idx} /> */}
+            {Array.from({ length: total }).map((_, idx) => {
+                const size = rnd(20, 70) / 10;
+                return (
+                    <BubbleChild key={idx}
+                        style={{
+                            left: `${rnd(20, 70)}%`,
+                            top: `${rnd(50, 70)}%`,
+                            width: `${size}px`,
+                            height: `${size}px`,
+                            animationDelay: `${-rnd(0, 30) / 10}s`
+                        }}
+                    />
+                );
+            })}
+        </div>
+    );
+}
--- src/pages/PageC/PageBlobs.tsx
+++ src/pages/PageC/PageBlobs.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { a, useTrail, Spring, config } from "@react-spring/web";
-import { atom, useAtom } from 'jotai';
-import styled, { keyframes } from 'styled-components';
+import { useAtom } from 'jotai';
+import styled from 'styled-components';
 import { useKey } from 'react-use';
 import { useGooAtom } from '../../store/store';
-import { rnd } from '../../utils/numbers';
+import { Bubbles } from './Bowl';
 
 const BLOBPOS = [{
     width: 120,
@@ -92,69 +92,6 @@ function FilterGoo() {
                 <feColorMatrix in="blur" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 30 -7" />
             </filter>
         </svg>
-    );
-}
-
-const bubblesAnim = keyframes`
-   0% {
-      opacity: 0;
-      background-color: teal;
-      scale: 0.1;
-   }
-   20% { //show and hint at moving
-      opacity: 0.25;
-      transform: translate(-30%, 40%);
-      scale: 0.5;
-   }
-   40% { //show and hint at moving
-      opacity: 1;
-      transform: translate(30%, -20%);
-      scale: 0.2;
-   }
-   60%{
-      background-color: #fff7;
-      transform: translate(-60%, -20%);
-      scale: 1;
-   }
-   100% {
-      opacity: 0;
-      transform: translate(0, -500%); //big bubbles move faster
-   }
-`;
-
-const BubbleChild = styled.div`
-    position: absolute;
-    //background-color: red;
-    background-color: #fff7;
-    opacity: 0;
-    border-radius: 100%;
-    animation: ${bubblesAnim} 3s ease-in infinite;
-`;
-
-function Bubbles() {
-    const [total, setTotal] = React.useState(10);
-    React.useEffect(() => {
-        const int = setInterval(() => setTotal(rnd(total - 1, total + 1)), 5000);
-        return () => clearInterval(int);
-    }, []);
-    return (
-        <div className="absolute left-[15.5%] top-[12.5%] w-[48.5%] h-[66%] overflow-hidden rounded-full bg-blue-600/20">
-            {/* <BubbleChild $left={rnd(10, 80)} $top={rnd(60, 80)} $size={rnd(40, 80) / 10} $delay={-rnd(0, 30) / 10} key={idx} /> */}
-            {Array.from({ length: total }).map((_, idx) => {
-                const size = rnd(20, 70) / 10;
-                return (
-                    <BubbleChild key={idx}
-                        style={{
-                            left: `${rnd(20, 70)}%`,
-                            top: `${rnd(50, 70)}%`,
-                            width: `${size}px`,
-                            height: `${size}px`,
-                            animationDelay: `${-rnd(0, 30) / 10}s`
-                        }}
-                    />
-                );
-            })}
-        </div>
     );
 }
 
