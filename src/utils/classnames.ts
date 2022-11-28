1116f40e044ba4fa97a4a2a052c9c1def2fd40bf
Max Zakharzhevskiy <maxzz@msn.com>
2022-11-27 23:32:10 -0800

    WIP. utils. udpate classnames() signature

--- src/utils/classnames.ts
+++ src/utils/classnames.ts
@@ -1,11 +1,15 @@
-export default function cx(...configs: Array<string | Record<string, boolean>>): string {
-    return configs.map((config) => (
-        typeof config === 'string'
-            ? config
-            : Object.keys(config).filter((subKey: string) => config[subKey]).join(' ')),
+export function cx(...classes: Array<string | Record<string, boolean>>): string {
+    return classes.map((cls) => (
+        typeof cls === 'string'
+            ? cls
+            : Object.keys(cls).filter((subKey: string) => cls[subKey]).join(' ')),
     ).join(' ');
 }
 
-export function classNames(...classes: string[]): string {
+export function classNames(...classes: (string | undefined | false | null | 0)[]): string {
     return classes.filter(Boolean).join(' ');
+}
+
+export function tw(s: string): string { // added to help tailwind intellisense
+    return s;
 }
