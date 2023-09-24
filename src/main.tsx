74eb15da59dcd84e983fa12552c717cdb95d6c71
Max Zakharzhevskiy <maxzz@msn.com>
2023-09-24 16:28:13 -0700

    WIP. update package dependencies. react 17->18

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,25 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { App } from './App';
+// import './index.css';
+
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>,
+//     document.getElementById('root')
+// );
+
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './index.css';
 
-ReactDOM.render(
+const domNode = document.getElementById('root')!;
+const root = createRoot(domNode);
+
+root.render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
