7ea93c26759921a7901b901a15c917d4eeac714a
Max Zakharzhevskiy <maxzz@msn.com>
2023-09-30 13:33:16 -0700

    WIP. add defferent timeout for enter, exit phases

--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -11,7 +11,6 @@ function MenuButton(props: LinkProps & React.RefAttributes<HTMLAnchorElement>) {
                     return `px-4 py-2 rounded shadow ${isActive ? 'bg-red-300' : 'opacity-75 border-2 border-red-600/10 hover:bg-red-300/60 active:scale-[.97]'}`;
                 }
             }
-            // exact={true}
             {...props}
         />
     );
@@ -40,21 +39,29 @@ function RoutesSelector() {
                     <CSSTransition
                         key={location.pathname}
                         nodeRef={nodeRef}
-                        timeout={300}
+                        //timeout={100} //timeout={300}
+                        timeout={{
+                            appear: 300,
+                            enter: 200,
+                            exit: 100,
+                        }}
                         classNames="fade"
                         unmountOnExit
                     >
                         {(state) => (
                             <div ref={nodeRef} className="page">
                                 {console.log(`transition state: ${state} page: "${location.pathname}"`)!}
-                                
+
                                 {currentOutlet}
                             </div>
                         )}
                     </CSSTransition>
                 </SwitchTransition>
-
             </div >
+
+            <div className="">
+                <a href="https://maxzz.github.io/react-spring-tests" target='_blank'>prev animations</a>
+            </div>
 
         </div>
     </>);
