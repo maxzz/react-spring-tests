30c49d721fd4f6332419bd7b0e698511f8266560
Max Zakharzhevskiy <maxzz@msn.com>
2024-01-17 20:41:50 -0800

    WIP. routes. update error page

--- src/components/pages/0-routes/2-404/index.tsx
+++ src/components/pages/0-routes/2-404/index.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 export function Page404() {
     return (
         <div className="h-screen bg-sky-400 grid place-items-center">
-            404
+            <div className="flex flex-col items-center gap-y-2">
+                <div className="text-2xl">404</div>
+                <div className="mb-8 text-2xl">Page not found</div>
+                <Link to="/" className="px-3 py-2 border-sky-600 border rounded shadow">Go Home</Link>
+            </div>
         </div>
     );
 }
