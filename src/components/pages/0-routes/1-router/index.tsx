90b3f0afe57bfd7374ebc9b6ade4c75f1a691cff
Max Zakharzhevskiy <maxzz@msn.com>
2024-01-17 17:02:01 -0800

    WIP. routes. add 1-router

---
+++ src/components/pages/0-routes/1-router/index.tsx
@@ -0,0 +1,57 @@
+import { useLocation, createBrowserRouter, useOutlet } from 'react-router-dom';
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
+import { routes } from '../all-routes';
+import { TopMenu } from '../0-top-menu';
+
+function Root() {
+    const location = useLocation();
+    const currentOutlet = useOutlet();
+    const { nodeRef } = routes.find((route) => route.path === location.pathname) ?? {};
+    return (<>
+        <div className="h-screen flex flex-col bg-[salmon] text-red-800">
+            <TopMenu />
+
+            <div className="flex-1 relative">
+                <SwitchTransition mode="out-in">
+                    <CSSTransition
+                        key={location.pathname}
+                        nodeRef={nodeRef}
+                        //timeout={100} //timeout={300}
+                        timeout={{
+                            appear: 300,
+                            enter: 200,
+                            exit: 100,
+                        }}
+                        classNames="fade"
+                        unmountOnExit
+                    >
+                        {(state) => (
+                            <div ref={nodeRef} className="page">
+                                {console.log(`transition state: ${state} page: "${location.pathname}"`)!}
+
+                                {currentOutlet}
+                            </div>
+                        )}
+                    </CSSTransition>
+                </SwitchTransition>
+            </div >
+
+            <div className="">
+                <a href="https://maxzz.github.io/react-spring-tests" target='_blank'>prev animations</a>
+            </div>
+
+        </div>
+    </>);
+}
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />,
+        children: routes.map((route) => ({
+            index: route.path === '/',
+            path: route.path,
+            element: route.element,
+        }))
+    }
+]);
--- src/components/pages/0-routes/index.tsx
+++ src/components/pages/0-routes/index.tsx
@@ -1,61 +1,6 @@
-import { useLocation, createBrowserRouter, RouterProvider, useOutlet } from 'react-router-dom';
-import { CSSTransition, SwitchTransition } from 'react-transition-group';
-import { routes } from './all-routes';
-import { TopMenu } from './0-top-menu';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './1-router';
 import './transitions-page.css';
-
-function Root() {
-    const location = useLocation();
-    const currentOutlet = useOutlet();
-    const { nodeRef } = routes.find((route) => route.path === location.pathname) ?? {};
-    return (<>
-        <div className="h-screen flex flex-col bg-[salmon] text-red-800">
-            <TopMenu />
-
-            <div className="flex-1 relative">
-                <SwitchTransition mode="out-in">
-                    <CSSTransition
-                        key={location.pathname}
-                        nodeRef={nodeRef}
-                        //timeout={100} //timeout={300}
-                        timeout={{
-                            appear: 300,
-                            enter: 200,
-                            exit: 100,
-                        }}
-                        classNames="fade"
-                        unmountOnExit
-                    >
-                        {(state) => (
-                            <div ref={nodeRef} className="page">
-                                {console.log(`transition state: ${state} page: "${location.pathname}"`)!}
-
-                                {currentOutlet}
-                            </div>
-                        )}
-                    </CSSTransition>
-                </SwitchTransition>
-            </div >
-
-            <div className="">
-                <a href="https://maxzz.github.io/react-spring-tests" target='_blank'>prev animations</a>
-            </div>
-
-        </div>
-    </>);
-}
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Root />,
-        children: routes.map((route) => ({
-            index: route.path === '/',
-            path: route.path,
-            element: route.element,
-        }))
-    }
-]);
 
 export function MainRouter() {
     return (<>
